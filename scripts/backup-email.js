81188a292b63f7c5c07b509afb20bf30e87d3d25
arturmichalow <arturmichalow@gmail.com>
2026-05-27 13:56:04 +0200

    Update backup-email.js

--- scripts/backup-email.js
+++ scripts/backup-email.js
@@ -25,14 +25,17 @@ Railway PostgreSQL działa poprawnie.
     console.log("Konfiguracja SMTP...");
 
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
+  host: process.env.SMTP_HOST,
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
     console.log("SMTP gotowe");
 
