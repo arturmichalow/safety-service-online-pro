e0d89dc85fbb1c2d345ef079a8d7acaf14e71252
arturmichalow <arturmichalow@gmail.com>
2026-07-27 09:38:03 +0200

    Add files via upload

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import 'leaflet/dist/leaflet.css';
 import './style.css';
 export const metadata = { title: 'Safety Service - Rozliczanie Obsługi Firm', manifest: '/manifest.json' };
 export default function RootLayout({ children }) { return <html lang="pl"><body>{children}</body></html>; }
